e906929905d9536488627d27347283815398312a
rodrigorgtic <rodrigo.rgtic@gmail.com>
2020-07-09 15:39:39 -0300

    feat: create user

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
 import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -56,7 +57,13 @@ const SignUp: React.FC = () => {
         abortEarly: false, // retorna todos os erros de  validação de uma vez só.
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
+
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.',
+      );
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
