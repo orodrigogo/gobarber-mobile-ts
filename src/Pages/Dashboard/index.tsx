72ba5b99965d1eb2ed103353501ef34fb35cb584
rodrigorgtic <rodrigo.rgtic@gmail.com>
2020-07-09 17:33:38 -0300

    feat: find providers

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native';
+import api from '../../services/api';
 
 import { useAuth } from '../../hooks/auth';
 
@@ -11,16 +12,30 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
-import Profile from '../Profile';
+
+export interface IProvider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<IProvider[]>([]);
+
   const { signOut, user } = useAuth();
   const { navigate } = useNavigation();
 
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
   }, [navigate]);
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
 
   return (
     <Container>
@@ -35,6 +50,12 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
       <Button title="Sair" onPress={signOut} />
     </Container>
   );
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -47,6 +47,9 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        // Inserindo e definindo o token para todas as requisições.
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -83,7 +86,7 @@ const AuthProvider: React.FC = ({ children }) => {
 
   const updatedUser = useCallback(
     (user: IUser) => {
-      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+      AsyncStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
       setData({
         token: data.token,
