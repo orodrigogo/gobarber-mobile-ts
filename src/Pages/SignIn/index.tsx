1c015a24bf8782745e6a350f04b527ab7f12081e
rodrigo <rodrigo.rgtic@gmail.com>
2020-07-09 10:25:51 -0300

    feat: set font roboto

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
 };
 
 export default SignIn;
