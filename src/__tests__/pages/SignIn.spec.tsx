f212c85c520fdd7bc600bcbd063d136e18d84557
rodrigorgtic <rodrigo.rgtic@gmail.com>
2020-07-10 17:40:34 -0300

    feat: apply testing library

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-native-testing-library';
+
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+
+describe('SignIn page', () => {
+  it('Should contains email/password inputs', () => {
+    const { getByPlaceholder } = render(<SignIn />);
+
+    expect(getByPlaceholder('E-mail')).toBeTruthy();
+    expect(getByPlaceholder('Senha')).toBeTruthy();
+  });
+});
